c9d280b3affd8d2df914c691b5338302146478ae
hanloveland <hanloveland1@gmail.com>
2025-03-04 02:08:37 +0900

    run gem5 with skylake configuraion (init)

---
+++ run/skylake-config/run-se.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2020 The Regents of the University of California
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met: redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer;
+# redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution;
+# neither the name of the copyright holders nor the names of its
+# contributors may be used to endorse or promote products derived from
+# this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# Authors: Jason Lowe-Power, Trivikram Reddy
+
+import sys 
+sys.path.append('/var/share/run_gem5/run_gem5/run/skylake-config/system')
+
+import m5
+from m5.objects import *
+import argparse
+from system.se  import MySystem
+from system.core import *
+
+# , UnConstrainedCPU
+valid_configs = [VerbatimCPU, TunedCPU]
+valid_configs = {cls.__name__[:-3]:cls for cls in valid_configs}
+
+parser = argparse.ArgumentParser()
+parser.add_argument('config', choices = valid_configs.keys())
+parser.add_argument('binary', type = str, help = "Path to binary to run")
+args = parser.parse_args()
+
+class TestSystem(MySystem):
+    _CPUModel = valid_configs[args.config]
+
+system = TestSystem()
+system.setTestBinary(args.binary)
+root = Root(full_system = False, system = system)
+m5.instantiate()
+
+exit_event = m5.simulate()
+
+print("Test!")
+if exit_event.getCause() != 'exiting with last active thread context':
+    print("Benchmark failed with bad exit cause.")
+    print(exit_event.getCause())
+    exit(1)
+if exit_event.getCode() != 0:
+    print("Benchmark failed with bad exit code.")
+    print("Exit code {}".format(exit_event.getCode()))
+    exit(1)
